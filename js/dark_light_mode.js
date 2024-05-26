8a5a29faf07ac6d5e8ac3ba98203bb578bcb73bc
root404 <mohammad>
2024-05-26 16:21:18 +0330

    fixed bug project and added the component

--- js/dark_light_mode.js
+++ js/dark_light_mode.js
@@ -1,25 +1,40 @@
 let lightMode = document.getElementById('lightModeBtn')
 let darkMode = document.getElementById('darkModeBtn')
+let lightModeAlert = document.getElementById('lightModeAlert')
+let darkModeAlert = document.getElementById('darkModeAlert')
 darkMode.addEventListener("click", function () {
     document.documentElement.setAttribute('data-bs-theme', 'dark')
-    darkMode.classList.add('active')
-    lightMode.classList.remove('active')
-    localStorage.setItem('themeMode', 'dark')
+    lightMode.classList.remove('d-none')
+    darkMode.classList.add('d-none')
+    lightModeAlert.classList.add('d-none')
+    darkModeAlert.classList.remove('d-none')
 })
 lightMode.addEventListener("click", function () {
     document.documentElement.setAttribute('data-bs-theme', 'light')
-    lightMode.classList.add('active')
-    darkMode.classList.remove('active')
-    localStorage.setItem('themeMode', 'dark')
+    darkMode.classList.remove('d-none')
+    lightMode.classList.add('d-none')
+    darkModeAlert.classList.add('d-none')
+    lightModeAlert.classList.remove('d-none')
 })
 
-let saveThemMode = localStorage.getItem('themeMode')
-if (saveThemMode == 'dark') {
-    document.documentElement.setAttribute('data-bs-theme', 'dark')
-    darkMode.classList.add('active')
-    lightMode.classList.remove('active')
-} else {
-    document.documentElement.setAttribute('data-bs-theme', 'light')
-    lightMode.classList.add('active')
-    darkMode.classList.remove('active')
+
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+const alertTrigger = document.getElementById('lightModeBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('شما در حال استفاده از حالت روشن هستید', 'success')
+    })
 }
