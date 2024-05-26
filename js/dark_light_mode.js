d7c14e090145388b845733f985d71064f4b23d08
root404 <mohammad>
2024-05-26 17:04:05 +0330

    fixed bugs

--- js/dark_light_mode.js
+++ js/dark_light_mode.js
@@ -16,25 +16,3 @@ lightMode.addEventListener("click", function () {
     darkModeAlert.classList.add('d-none')
     lightModeAlert.classList.remove('d-none')
 })
-
-
-
-
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-    const wrapper = document.createElement('div')
-    wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-        `   <div>${message}</div>`,
-        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-    ].join('')
-
-    alertPlaceholder.append(wrapper)
-}
-const alertTrigger = document.getElementById('lightModeBtn')
-if (alertTrigger) {
-    alertTrigger.addEventListener('click', () => {
-        appendAlert('شما در حال استفاده از حالت روشن هستید', 'success')
-    })
-}
